d30d54b48c3cae75cf65b17c7905d53da74e82f7
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-15 09:09:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhithemachinegun/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/Rokstar890/EasyAutoFilter.git /EasyAutoFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EasyAdvAutoFilter
+  git clone $UPSTREAM_REPO /EasyAutoFilter
 fi
 cd /spidyauto
 pip3 install -U -r requirements.txt
